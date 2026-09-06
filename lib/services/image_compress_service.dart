96040d54934cf70911eea1ffc1f661f29c2bfb74
Deepak Kumar Mahanta <dipu865862@gmail.com>
2026-09-06 16:39:17 +0530

    Update image_compress_service.dart

--- lib/services/image_compress_service.dart
+++ lib/services/image_compress_service.dart
@@ -2,13 +2,12 @@ import 'dart:io';
 import 'package:flutter_image_compress/flutter_image_compress.dart';
 
 class ImageCompressService {
-  // Image ka size kam karne ka function
   static Future<File?> compressImage(File file) async {
     try {
       final result = await FlutterImageCompress.compressAndGetFile(
         file.absolute.path,
         file.parent.path + '/compressed_${file.path.split('/').last}',
-        quality: 80, // 80% quality maintain karega taaki size kam ho
+        quality: 80,
       );
 
       if (result != null) {
@@ -17,7 +16,7 @@ class ImageCompressService {
       return null;
     } catch (e) {
       print('Image compression error: $e');
-      return file; // Agar compress na ho, toh original file wapas bhej do taaki app crash na ho
+      return file;
     }
   }
 }
